52c1a276bdb7a96c041e2f9da84df70f0f69232b
FuneralClown13 <funeralclown@yandex.ru>
2023-07-31 14:10:33 +0200

    add hw

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+def test_existence_add_to_basket_btn(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    btn_list = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert len(btn_list) >= 1
